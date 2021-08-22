8bae84eaa642333a18913c696f22dae982b815a4
Andreas Knoben <mail@andreasknoben.nl>
2021-08-22 13:05:13 +0200

    Add bootstrap files, create index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- Stylesheets -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        
+        <!-- JavaScript -->
+        <script src="js/bootstrap.bundle.min.js"></script>
+    </head>
+    
+    <body>
+        <p>Hello world</p>
+    </body>
+</html>
