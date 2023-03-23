79b3933fb16bf1256eaf58bf18a8c3cd309dc59a
Andreas Knoben <mail@andreasknoben.nl>
2023-03-23 23:55:10 +0100

    Completed functions list

--- app/funcs.py
+++ app/funcs.py
@@ -3,6 +3,9 @@ import pandas as pd
 # Classifiers
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 
 
 def data_statistics(filename):
@@ -24,10 +27,33 @@ def get_algorithm_parms(alg):
             return {'function': KNeighborsClassifier,
                     'funcname': "K-Nearest Neighbours Classifier",
                     'parms': parms}
+        
         case 'logreg':
             parms = [('penalty', ['l1', 'l2', 'none']),
                      ('C', 'float')]
             return {'function': LogisticRegression,
                     'funcname': "Logistic Regression",
                     'parms': parms}
-    return alg
+        
+        case 'svm':
+            parms = [('C', 'float'),
+                     ('degree', 'int')]
+            return {'function': SVC,
+                    'funcname': "Support Vector Classifier",
+                    'parms': parms}
+        
+        case 'dt':
+            parms = [('max_depth', 'int')]
+            return {'function': DecisionTreeClassifier,
+                    'funcname': "Decision Tree Classifier",
+                    'parms': parms}
+        
+        case 'rf':
+            parms = [('n_estimators', 'int'),
+                     ('max_depth', 'int')]
+            return {'function': RandomForestClassifier,
+                    'funcname': "Random Forest Classifier",
+                    'parms': parms}
+        
+        case _:
+            return ValueError()
